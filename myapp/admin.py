6d7f45e8ec061dfe503aded1efad4738289ba46a
Levi Nguyen <hn112000@gmail.com>
2021-01-13 15:50:57 +0700

    add admin site

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,22 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from myapp.models import Customer, User, Product, Brand
 
+# @admin.register(Customer)
+# class CustomerAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     pass
 
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    pass
+#
+# @admin.register(Brand)
+# class BrandAdmin(admin.ModelAdmin):
+#     pass
 
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Brand)
-class BrandAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Brand)
